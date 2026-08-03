4065f114c0d20e3ac1470f25189f2d9496f7afbe
Olatunde328 <olagokejohn57@yahoo.com>
2026-08-03 02:21:12 +0100

    Remove corrupted milestone dropdown characters

--- client/src/components/EvidenceForm.jsx
+++ client/src/components/EvidenceForm.jsx
@@ -135,7 +135,7 @@ function EvidenceForm({
                 value={milestone.id}
                 disabled={milestone.paid}
               >
-                {milestone.title} Ã‚Â· {milestone.status}
+                {milestone.title}
               </option>
             ))}
           </select>
